f3aa67b104379f58452c1e2c522becd8a99ee338
appalse <frontend-hh@yandex.ru>
2021-12-09 12:14:14 +0100

    change title and favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 
 function App() {
+  useEffect(() => {
+      document.title = 'Hadn Dadn Tribute Page'
+  }, []);
+
   return (
     <div className="App">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
