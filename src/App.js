ac1d39ce3e0f4bfac30c69a555f0c12b5bf2858d
appalse <frontend-hh@yandex.ru>
2021-12-21 15:30:50 +0300

    add translation for footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react'
 import './App.css';
 import Header from "./header";
+import { useTranslation } from "react-i18next";
 import MainContent from "./mainContent";
 import Footer from "./footer";
+import LanguageSelect from "./languageSelect";
 
 function App() {
   useEffect(() => {
       document.title = "Hadn Dadn"
   }, []);
+
+  const { t } = useTranslation();
 
   return (
       <div className="main">
--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,14 @@
 import React from "react";
-//import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 
 const Footer = () => {
-    //const { t } = useTranslation();
 
     return (
-        <footer className="footer">This webpage is created by <a className="footer__link_text-color_special-blue" href="https://www.appalse.ru/" target="_blank" rel="noopener noreferrer">appalse</a>, <time dateTime="2021">2021</time></footer>
+        <footer className="footer">
+            <Trans i18nKey="this_webpage_created">
+                This webpage is created by <a className="footer__link_text-color_special-blue" href="https://www.appalse.ru/" target="_blank" rel="noopener noreferrer">appalse</a>, <time dateTime="2021">2021</time>
+            </Trans>
+        </footer>
     );
 };
 
