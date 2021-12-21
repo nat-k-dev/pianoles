b9bd59faa371b49fedaf80a8077410ed56392d68
appalse <frontend-hh@yandex.ru>
2021-12-22 00:49:39 +0300

    new style with neon

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header.css";
+import "./header__title_text_neon.css";
 import { useTranslation } from "react-i18next";
 import LanguageSelect from "./__language-select/languageSelect";
 
@@ -11,8 +12,8 @@ const Header = () => {
           <div className="header__language-select">
             <LanguageSelect />
           </div>
-          <h1 className="header__title header__title_font_important">{t("hadn_dadn")}</h1>
-          <p className="header__description header__description_font_800">{t("moscow_music_group")}</p>
+          <h1 className="header__title header__title_font_big-caveat header__title_text_neon">{t("hadn_dadn")}</h1>
+          <p className="header__description header__description_font_small-caveat">{t("moscow_music_group")}</p>
         </header>
     );
 };
