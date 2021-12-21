321520394ad4a53551a5bcb46250f7330ab66473
appalse <frontend-hh@yandex.ru>
2021-12-22 01:40:50 +0300

    fix className instead of class

--- src/common.blocks/main-content/__article/article.js
+++ src/common.blocks/main-content/__article/article.js
@@ -53,22 +53,22 @@ const Article = () => {
                 </li>
                 <li>
                   <Trans i18nKey="2017_secret_album">
-                    <time dateTime="2017">2017</time> - <a class="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%A2%D0%B0%D0%B9%D0%BD%D1%8B%D0%B9+%D0%B0%D0%BB%D1%8C%D0%B1%D0%BE%D0%BC" target="_blank" rel="noopener noreferrer">Secret album</a>
+                    <time dateTime="2017">2017</time> - <a className="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%A2%D0%B0%D0%B9%D0%BD%D1%8B%D0%B9+%D0%B0%D0%BB%D1%8C%D0%B1%D0%BE%D0%BC" target="_blank" rel="noopener noreferrer">Secret album</a>
                   </Trans>
                 </li>
                 <li>
                   <Trans i18nKey="2018_girlfriends">
-                    <time dateTime="2018">2018</time> - <a class="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9F%D0%BE%D0%B4%D1%80%D1%83%D0%B6%D0%BA%D0%B8" target="_blank" rel="noopener noreferrer">Girlfriends</a>
+                    <time dateTime="2018">2018</time> - <a className="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9F%D0%BE%D0%B4%D1%80%D1%83%D0%B6%D0%BA%D0%B8" target="_blank" rel="noopener noreferrer">Girlfriends</a>
                   </Trans>
                 </li>
                 <li>
                   <Trans i18nKey="2019_liaoakyn">
-                    <time dateTime="2019">2019</time> - <a class="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9B%D1%8F%D0%BE%D0%B0%D0%BA%D1%8B%D0%BD" target="_blank" rel="noopener noreferrer">Liaoakyn</a>
+                    <time dateTime="2019">2019</time> - <a className="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9B%D1%8F%D0%BE%D0%B0%D0%BA%D1%8B%D0%BD" target="_blank" rel="noopener noreferrer">Liaoakyn</a>
                   </Trans>
                 </li>
                 <li>
                   <Trans i18nKey="2020_nostalgia">
-                    <time dateTime="2020">2020</time> - <a class="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9D%D0%BE%D1%81%D1%82%D0%B0%D0%BB%D1%8C%D0%B3%D0%B8%D1%8F" target="_blank" rel="noopener noreferrer">Nostalgia</a>
+                    <time dateTime="2020">2020</time> - <a className="link link__text-color_special-purple" href="https://www.last.fm/ru/music/%D0%A5%D0%B0%D0%B4%D0%BD+%D0%B4%D0%B0%D0%B4%D0%BD/%D0%9D%D0%BE%D1%81%D1%82%D0%B0%D0%BB%D1%8C%D0%B3%D0%B8%D1%8F" target="_blank" rel="noopener noreferrer">Nostalgia</a>
                   </Trans>
                 </li>
               </ul>
@@ -77,7 +77,7 @@ const Article = () => {
               <h3 className="article__subtitle">{t("more_information")}</h3>
               <p className="article__paragraph paragraph">
                 <Trans i18nKey="you_could_read_more">
-                  You could read more about Hadn Dadn on <a class="link link__text-color_special-purple" target="_blank" rel="noopener noreferrer" href="https://ru.wikipedia.org/wiki/%D0%A5%D0%B0%D0%B4%D0%BD_%D0%B4%D0%B0%D0%B4%D0%BD">Wikipedia</a>.
+                  You could read more about Hadn Dadn on <a className="link link__text-color_special-purple" target="_blank" rel="noopener noreferrer" href="https://ru.wikipedia.org/wiki/%D0%A5%D0%B0%D0%B4%D0%BD_%D0%B4%D0%B0%D0%B4%D0%BD">Wikipedia</a>.
                 </Trans>
               </p>
             </section>
