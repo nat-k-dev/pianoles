c8a27ec0bdca938f0ac1279783fff339b1dc6245
appalse <frontend-hh@yandex.ru>
2022-03-21 14:45:36 +0100

    remove some links

--- src/common.blocks/social-media-list/socialMediaList.js
+++ src/common.blocks/social-media-list/socialMediaList.js
@@ -6,8 +6,6 @@ const SocialMediaList = () => {
         <ul className="article__social-media-list social-media-list">
             <li><a target="_blank" rel="noopener noreferrer" className="fa fa-youtube" href="https://www.youtube.com/channel/UCNWI_pyxZk0xbPTfgTl_P-A"><div className="sr-only">Youtube</div></a></li>
             <li><a target="_blank" rel="noopener noreferrer" className="fa fa-vk" href="https://vk.com/hadndadn"><div className="sr-only">VKontakte</div></a></li>
-            <li><a target="_blank" rel="noopener noreferrer" className="fa fa-facebook" href="https://www.facebook.com/hadndadn/"><div className="sr-only">Facebook</div></a></li>
-            <li><a target="_blank" rel="noopener noreferrer" className="fa fa-instagram" href="https://www.instagram.com/hadn_dadn/?hl=ru"><div className="sr-only">Instagram</div></a></li>
         </ul>
     );
 };
