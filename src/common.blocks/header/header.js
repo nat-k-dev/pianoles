d223ccd829fe773037e246d40abd1a3ec60bf6ca
appalse <frontend-hh@yandex.ru>
2022-08-24 22:53:36 +0200

    remove neon animation lights

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./header.css";
-import "./header__title_text_neon.css";
 import { useTranslation } from "react-i18next";
 import LanguageSelect from "./__language-select/languageSelect";
 
@@ -12,7 +11,7 @@ const Header = () => {
           <div className="header__language-select">
             <LanguageSelect />
           </div>
-          <h1 className="header__title header__title_font_big-caveat header__title_text_neon">{t("header_h1_title")}</h1>
+          <h1 className="header__title header__title_font_big-caveat">{t("header_h1_title")}</h1>
           <p className="header__description header__description_font_small-caveat">{t("moscow_music_group")}</p>
         </header>
     );
