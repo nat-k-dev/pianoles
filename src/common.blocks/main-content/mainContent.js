8f228f5d152386088224abda7ed1cbf71ebfe70e
appalse <frontend-hh@yandex.ru>
2022-08-24 22:31:06 +0200

    replace main photo

--- src/common.blocks/main-content/mainContent.js
+++ src/common.blocks/main-content/mainContent.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Trans } from "react-i18next";
 import "./mainContent.css";
 import Article from "./__article/article";
+import mainContentImage from '../../assets/img/look-from-mother-daughter-playing-white-piano_1275_850.jpg';
 
 const MainContent = () => {
     return (
         <main className='main__main-content main-content main-content_background_light'>
           <figure>
-              <a href="https://commons.wikimedia.org/wiki/File:%D0%A5%D0%B0%D0%B4%D0%BD_%D0%B4%D0%B0%D0%B4%D0%BD_2020.jpg#/media/Файл:Хадн_дадн_2020.jpg">
-                <img className="main-content__image" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/%D0%A5%D0%B0%D0%B4%D0%BD_%D0%B4%D0%B0%D0%B4%D0%BD_2020.jpg" alt="Hadn Dadn on stage"/>
+              <a href="https://www.freepik.com/free-photo/look-from-mother-daughter-playing-white-piano_1120622.htm#query=woman%20playing%20piano&position=1&from_view=keyword">
+                <img className="main-content__image" src={mainContentImage} alt="look from mother daughter playing white piano"/>
               </a>
               <figcaption className="main-content__image-caption image-caption image-caption_text_small image-caption_text_italic">
                 <Trans i18nKey="figcaption_img_caption">
-                  Hand Dadn are on the stage on <time dateTime="2020-09-06">September 6, 2020</time>, Author: Kerrin Key
+                  Piano lesson for adults and children, image by v.ivash from freepik.com
                 </Trans>
               </figcaption>
           </figure>
