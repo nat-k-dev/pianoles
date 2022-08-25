2afeb926bcc089fbbc8a5546ead78d83d447200a
appalse <frontend-hh@yandex.ru>
2022-08-25 11:50:35 +0200

    change header styles

--- src/common.blocks/header/__language-select/languageSelect.js
+++ src/common.blocks/header/__language-select/languageSelect.js
@@ -23,7 +23,7 @@ const LanguageSelect = () => {
 
   return (
     <div className="d-flex justify-content-end align-items-center language-select-root language-select">
-      <Button style={{ color: "plum" }} onClick={({ currentTarget }) => setMenuAnchor(currentTarget)}>
+      <Button onClick={({ currentTarget }) => setMenuAnchor(currentTarget)}>
         {languageMap[selected].label}
         <ArrowDropDown fontSize="small" />
       </Button>
--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -8,10 +8,12 @@ const Header = () => {
 
     return (
         <header className="header">
-          <div className="header__language-select">
-            <LanguageSelect />
+          <div className="header__container">
+            <h1 className="header__title header__title_font_big-caveat">{t("header_h1_title")}</h1>
+            <div className="header__language-select">
+              <LanguageSelect />
+            </div>
           </div>
-          <h1 className="header__title header__title_font_big-caveat">{t("header_h1_title")}</h1>
           <p className="header__description header__description_font_small-caveat">{t("moscow_music_group")}</p>
         </header>
     );
